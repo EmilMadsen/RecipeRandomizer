3e3d049b944df497c579704fb2a30024aff79252
MarianneMoerk <marianne.moerk@gmail.com>
2016-05-12 10:20:16 +0200

    added directive og sat det paa submitknappen

---
+++ app/directives.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mork on 10/05/2016.
+ */
+angular.module("randomRecipe") //reference to internship module
+    .directive("enterToSubmit", function(){
+        return function (scope, element, attrs){ //Link funktionen (scope er fra den controller der er i kontrol hvor directive indsættes)
+
+            //Key codes - A number which represents an actual key on the keyboard
+            element.bind("keydown keypress", function (event) { //element The keydown event occurs when the key is pressed, followed immediately by the keypress event. Then the keyup event is generated when the key is released.
+                // Add which for key events
+                //if ( event.which === null && (event.charCode !== null || event.keyCode !== null) ) {
+                 //   event.which = event.charCode !== null ? event.charCode : event.keyCode;
+                //}
+                if(event.which === 13) {
+                 scope.$apply(function (){
+                     scope.$eval(attrs.enterToSubmit);
+                 });
+
+                 event.preventDefault();
+                }
+            });
+        }
+
+    });
